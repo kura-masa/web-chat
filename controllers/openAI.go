f610decdc7d1f6040339d7b6d42d9bed495ee9c7
kura-masa <82304043+kura-masa@users.noreply.github.com>
2024-01-30 22:31:55 +0900

    Update openAI.go

--- controllers/openAI.go
+++ controllers/openAI.go
@@ -37,7 +37,7 @@ func OpenAIContextTranslate(c *gin.Context) {
 			question = "B:" + chat.Content + question
 		}
 	}
-	question += chat_history[0].Content + "の目的語が無い場合補完した英文のみを出力して"
+	question += "というチャットの'" + chat_history[0].Content + "'の目的語が無い場合,補完した英文を出力して"
 
 	println("AIに送った質問の内容")
 	println(question)
